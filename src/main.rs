5710ec0cf64b7aad7eb95c65372c3ba619c4a7fb
Daniel Beach <danielbeach@Daniels-MacBook-Pro.local>
2023-06-28 09:23:57 -0500

    update code to run

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::collections::VecDeque;
 use csv::ReaderBuilder;
 use std::fs::File;
 
-
+#[derive(Clone, Debug, PartialEq)]
 struct Node {
     id: String,
     edge: String,
@@ -15,24 +15,23 @@ struct Node {
 
 #[tokio::main]
 async fn main() {
-    let file_stream: ByteStream = graphrs::download_object(
-        "divvy-tripdata", 
-        "202303-divvy-tripdata.zip").await;
-    let data: Bytes= file_stream.collect().await.map(|data| data.into_bytes()).expect("error reading data");
-    graphrs::write_bytes_to_zip_file("bikes.zip", data);
+    //let file_stream: ByteStream = graphrs::download_object(
+    //    "divvy-tripdata", 
+    //    "202303-divvy-tripdata.zip").await;
+    //let data: Bytes= file_stream.collect().await.map(|data| data.into_bytes()).expect("error reading data");
+    //graphrs::write_bytes_to_zip_file("bikes.zip", data);
     // zip file will be auto extracted into data folder
-    let df: DataFrame = graphrs::read_csv("data/202303-divvy-tripdata.csv").expect("error reading csv");
-    let dirty_graph: DataFrame = df.select(["start_station_id", "end_station_id"]).expect("blah");
-    let mut graph: DataFrame = dirty_graph.unique(None, UniqueKeepStrategy::Any, None).expect("blah blah");
-    let mut out: DataFrame = graphrs::combine_distinct_dataframes(&mut graph);
-    graphrs::write_dataframe_to_csv(&mut out, "nodes.csv");
-    graphrs::write_dataframe_to_csv(&mut graph, "edges.csv");
+    //let df: DataFrame = graphrs::read_csv("data/202303-divvy-tripdata.csv").expect("error reading csv");
+    //let dirty_graph: DataFrame = df.select(["start_station_id", "end_station_id"]).expect("blah");
+    //let mut graph: DataFrame = dirty_graph.unique(None, UniqueKeepStrategy::Any, None).expect("blah blah");
+    //let mut out: DataFrame = graphrs::combine_distinct_dataframes(&mut graph);
+    //graphrs::write_dataframe_to_csv(&mut out, "nodes.csv");
+    //graphrs::write_dataframe_to_csv(&mut graph, "edges.csv");
     // read nodes with Pythons script to visualize graph
 
     let file: File = File::open("edges.csv").expect("error opening file");
     let mut reader = ReaderBuilder::new().from_reader(file);
 
-    // Read and print each column
     let mut column_1: Vec<String> = Vec::new();
     let mut column_2: Vec<String> = Vec::new();
     let mut queue: VecDeque<Node> = VecDeque::new();
@@ -51,30 +50,24 @@ async fn main() {
 
     let start_node = Node { id: "20119".to_string(), edge: "".to_string() };
     let end_node = Node { id: "20104".to_string(), edge: "".to_string() };
-    queue.push_back(start_node.clone());
-    visited.insert(start_node.clone(), None);
-
-    while let Some(current_node) = queue.pop_front() {
-        if &current_node == end {
-            let mut path = Vec::new();
-            let mut current = &current_node;
-            while let Some(node) = visited.get(current) {
-                path.push(current.id.clone());
-                current = node.as_ref().unwrap();
-            }
-            path.push(start.id.clone());
-            path.reverse();
-
-            return Some((path, path.len() - 1));
-        }
-
-        if let Some(neighbors) = graph.get(&current_node) {
-            for neighbor in neighbors {
-                if !visited.contains_key(neighbor) {
-                    visited.insert(neighbor.clone(), Some(current_node.clone()));
-                    queue.push_back(neighbor.clone());
-                }
-            }
+    // find the shortest path from start_node to end_node
+    let mut path: Vec<String> = Vec::new();
+    let mut current_node = start_node.clone();
+    let mut found = false;
+    while !found {
+        let next_node = queue.pop_front().unwrap();
+        if next_node.id == end_node.id {
+            found = true;
+            path.push(next_node.id.clone());
+            path.push(next_node.edge.clone());
+            path.push(current_node.id.clone());
+        } else if next_node.id == current_node.id {
+            path.push(next_node.id.clone());
+            path.push(next_node.edge.clone());
+            current_node = Node{id: next_node.edge.clone(), edge: next_node.id.clone()};
+        } else {
+            queue.push_back(next_node);
         }
     }
+    println!("{:?}", path)
 }
